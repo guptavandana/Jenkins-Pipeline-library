ba256e38b86b98dfd89bc339ec1298f59339c2be
guptavandana <guptavandana7@gmail.com>
2018-02-01 15:55:56 +0800

    Added Jenkins file and groovy files

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -5,25 +5,25 @@ def call(body) {
    body()
 
    usePodTemplates{
-           stage('Get a Maven project') {
-            git 'https://github.com/jenkinsci/kubernetes-plugin.git'
-            container('maven') {
-                     stage('Build a Maven project') {
-                     sh 'mvn -B clean install'
-                     }
-            }
-            }
-            stage('Get a Golang project') {
-             git url: 'https://github.com/hashicorp/terraform.git'
-             container('golang') {
-                     stage('Build a Go project') {
-                    sh """
-                    mkdir -p /go/src/github.com/hashicorp
-                    ln -s `pwd` /go/src/github.com/hashicorp/terraform
-                    cd /go/src/github.com/hashicorp/terraform && make core-dev
-                    """
-                    }
-            }
-     }
-}
+	   stage('Get a Maven project') {
 
+		container('maven') {
+			stage('Build a Maven project') {
+				 sh 'mvn -version'
+			}
+		}
+		}
+		stage('Get a Golang project') {
+		 git url: 'https://github.com/hashicorp/terraform.git'
+		 container('golang') {
+				stage('Build a Go project') {
+					sh """
+						mkdir -p /go/src/github.com/hashicorp
+						ln -s `pwd` /go/src/github.com/hashicorp/terraform
+						cd /go/src/github.com/hashicorp/terraform && make core-dev
+					"""
+				}
+			}
+		}
+	}
+}
