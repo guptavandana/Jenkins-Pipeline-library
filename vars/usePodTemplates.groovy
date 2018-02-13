2bfb411dd89665ebab47c8081e621822b32aeb4a
guptavandana <guptavandana7@gmail.com>
2018-02-13 15:26:43 +0800

    test

--- vars/usePodTemplates.groovy
+++ vars/usePodTemplates.groovy
@@ -3,7 +3,8 @@ def call(String label, Closure body) {
 
 	podTemplate(label: label, containers: [
 		containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat'),
-		containerTemplate(name: 'golang', image: 'golang:1.8.0', ttyEnabled: true, command: 'cat')
+		containerTemplate(name: 'golang', image: 'golang:1.8.0', ttyEnabled: true, command: 'cat'),
+		containerTemplate( name: 'kubectl', image: 'roffe/kubectl:latest', ttyEnabled: true, command: 'cat')
 	  ]) {
 		node(label) {
 		  body()
